f654e9c99de058ad42c8e5b223bed3aef8553cc8
talentueuxbaby-afk <talentueuxbaby@gmail.com>
2026-09-11 08:55:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const bcrypt = require("bcryptjs");
 const Database = require("better-sqlite3");
 const path = require("path");
-
+const db = require("./db");
 const app = express();
 const PORT = Number(process.env.PORT || 3000);
 
